d95047cd26d9d8b6e5d9585ef1a481731515443b
Victor Gram Thomsen <31857458+grammeaway@users.noreply.github.com>
2019-04-09 11:45:08 +0200

    Basic bundle setup, lots of meta files

---
+++ RacingGame/NPC/src/main/java/racing/npc/NPCPlugin.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package racing.npc;
+
+import racing.common.data.GameData;
+import racing.common.data.World;
+import racing.common.services.IGamePluginService;
+
+/**
+ *
+ * @author Victor Gram
+ */
+public class NPCPlugin implements IGamePluginService {
+
+    @Override
+    public void start(GameData gameData, World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ RacingGame/NPC/src/main/java/racing/npc/NPCProcessingSystem.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package racing.npc;
+
+import racing.common.data.GameData;
+import racing.common.data.World;
+import racing.common.services.IEntityProcessingService;
+
+/**
+ *
+ * @author Victor Gram
+ */
+public class NPCProcessingSystem implements IEntityProcessingService {
+
+    @Override
+    public void process(GameData gameData, World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

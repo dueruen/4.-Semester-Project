a84dc9549ae0f759624bb3db2844348028880a30
kvartborg <hello@kvartborg.net>
2019-04-21 11:16:54 +0200

    Add player from lab examples

--- RacingGame/Common/src/main/java/racing/common/data/entityparts/EntityPart.java
+++ RacingGame/Common/src/main/java/racing/common/data/entityparts/EntityPart.java
@@ -1,12 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package racing.common.data.entityparts;
 
 import racing.common.data.Entity;
 import racing.common.data.GameData;
 
 /**
- * Entity part interface
- * 
+ *
+ * @author Alexander
  */
 public interface EntityPart {
+
     void process(GameData gameData, Entity entity);
 }
--- RacingGame/Common/src/main/java/racing/common/data/entityparts/PositionPart.java
+++ RacingGame/Common/src/main/java/racing/common/data/entityparts/PositionPart.java
@@ -4,8 +4,8 @@ import racing.common.data.Entity;
 import racing.common.data.GameData;
 
 /**
- * Object containing information about a position, based on a 2D 
- * 
+ * Object containing information about a position, based on a 2D
+ *
  */
 public class PositionPart implements EntityPart {
 
@@ -13,12 +13,12 @@ public class PositionPart implements EntityPart {
      * The x
      */
     private float x;
-    
+
     /**
      * The y
      */
     private float y;
-    
+
     /**
      * The rotation
      */
@@ -41,11 +41,11 @@ public class PositionPart implements EntityPart {
     public float getRadians() {
         return radians;
     }
-    
+
     public void setX(float newX) {
         this.x = newX;
     }
-    
+
     public void setY(float newY) {
         this.y = newY;
     }
@@ -61,5 +61,5 @@ public class PositionPart implements EntityPart {
 
     @Override
     public void process(GameData gameData, Entity entity) {
-    }    
+    }
 }

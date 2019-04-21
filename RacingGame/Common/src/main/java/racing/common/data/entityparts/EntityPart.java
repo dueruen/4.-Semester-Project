df362a5fd43bf658b9b56f3ba709a36d11a219f2
kvartborg <hello@kvartborg.net>
2019-04-21 11:18:52 +0200

    Remove authors and license headers

--- RacingGame/Common/src/main/java/racing/common/data/entityparts/EntityPart.java
+++ RacingGame/Common/src/main/java/racing/common/data/entityparts/EntityPart.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package racing.common.data.entityparts;
 
 import racing.common.data.Entity;
 import racing.common.data.GameData;
 
-/**
- *
- * @author Alexander
- */
 public interface EntityPart {
 
     void process(GameData gameData, Entity entity);
--- RacingGame/Common/src/main/java/racing/common/data/entityparts/MovingPart.java
+++ RacingGame/Common/src/main/java/racing/common/data/entityparts/MovingPart.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package racing.common.data.entityparts;
 
 import racing.common.data.Entity;
@@ -14,10 +9,6 @@ import static java.lang.Math.cos;
 import static java.lang.Math.sin;
 import static java.lang.Math.sqrt;
 
-/**
- *
- * @author Alexander
- */
 public class MovingPart
         implements EntityPart {
 
@@ -91,7 +82,7 @@ public class MovingPart
             radians -= rotationSpeed * dt;
         }
 
-        // accelerating            
+        // accelerating
         if (up) {
             dx += cos(radians) * acceleration * dt;
             dy += sin(radians) * acceleration * dt;

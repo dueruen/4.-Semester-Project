5f2bfa6ba1f6e52414631191f29b7d588e70a8f5
kvartborg <hello@kvartborg.net>
2019-05-07 11:31:33 +0200

    working collision system

--- RacingGame/Collision/src/main/java/racing/collision/CollisionSystem.java
+++ RacingGame/Collision/src/main/java/racing/collision/CollisionSystem.java
@@ -3,56 +3,64 @@ package racing.collision;
 import java.awt.geom.Area;
 import java.awt.Rectangle;
 import java.awt.Shape;
+import java.awt.geom.AffineTransform;
 import java.lang.Math;
+import racing.common.player.Player;
+import racing.common.map.Tile;
 import racing.common.data.Entity;
 import racing.common.data.GameData;
 import racing.common.data.GameImage;
 import racing.common.data.World;
 import racing.common.data.entityparts.MovingPart;
 import racing.common.data.entityparts.PositionPart;
+import racing.common.data.entityparts.TilePart;
 import racing.common.services.IPostEntityProcessingService;
 import java.util.UUID;
+
 
 public class CollisionSystem implements IPostEntityProcessingService {
 
     @Override
     public void process(GameData gameData, World world) {
-        for (Entity e : world.getEntities()) {
-            for (Entity f : world.getEntities()) {
-                if (e.getID().equals(f.getID())) {
-                   continue;
-                }
+        Player player = (Player) world.getEntities(Player.class).get(0);
+        MovingPart playerMovingPart = player.getPart(MovingPart.class);
 
-                PositionPart positionA = e.getPart(PositionPart.class);
-                Area a = new Area(new Rectangle(
-                    Math.round(e.getImage().getWidth()),
-                    Math.round(e.getImage().getWidth()),
-                    Math.round(positionA.getX()),
-                    Math.round(positionA.getY())
-                ));
+        for (Entity entity : world.getEntities(Tile.class)) {
+            TilePart tilePart = entity.getPart(TilePart.class);
 
-                PositionPart positionB = f.getPart(PositionPart.class);
-                Area b = new Area(new Rectangle(
-                    Math.round(f.getImage().getWidth()),
-                    Math.round(f.getImage().getWidth()),
-                    Math.round(positionB.getX()),
-                    Math.round(positionB.getY())
-                ));
-
-                a.intersect(b);
-
-                if (a.isEmpty()) {
-                    continue;
-                }
-
-                MovingPart mA = e.getPart(MovingPart.class);
-
-                if (mA == null) {
-                    continue;
-                }
-
-                mA.setAcceleration(0);
+            if (!tilePart.getType().isIsStatic()) {
+                continue;
             }
+
+            PositionPart playerPosition = player.getPart(PositionPart.class);
+
+            AffineTransform transform = new AffineTransform();
+            transform.rotate(
+                playerPosition.getRadians(),
+                playerPosition.getX() + player.getImage().getWidth()/2,
+                playerPosition.getY() + player.getImage().getHeight()/2
+            );
+
+            Shape playerShape = transform.createTransformedShape(new Rectangle(
+                Math.round(playerPosition.getX()),
+                Math.round(playerPosition.getY()),
+                Math.round(player.getImage().getWidth()),
+                Math.round(player.getImage().getHeight())
+            ));
+
+            PositionPart tilePosition = entity.getPart(PositionPart.class);
+            Rectangle tileShape = new Rectangle(
+                Math.round(tilePosition.getX()),
+                Math.round(tilePosition.getY()),
+                Math.round(entity.getImage().getWidth()),
+                Math.round(entity.getImage().getHeight())
+            );
+
+            if (!playerShape.intersects(tileShape)) {
+                continue;
+            }
+
+            playerMovingPart.setSpeed(0);
         }
     }
 }

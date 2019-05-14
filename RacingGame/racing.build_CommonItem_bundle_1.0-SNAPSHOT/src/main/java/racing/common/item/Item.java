446c7562b6901341cc608133fde1dfc64285adb9
GGkarma <32382976+GGkarma@users.noreply.github.com>
2019-05-14 10:01:29 +0200

    fix Item SPI

---
+++ RacingGame/racing.build_CommonItem_bundle_1.0-SNAPSHOT/src/main/java/racing/common/item/Item.java
@@ -0,0 +1,6 @@
+package racing.common.item;
+
+import racing.common.data.Entity;
+
+
+public class Item extends Entity { }
---
+++ RacingGame/racing.build_CommonItem_bundle_1.0-SNAPSHOT/src/main/java/racing/common/item/ItemSPI.java
@@ -0,0 +1,6 @@
+package racing.common.item;
+
+
+public interface ItemSPI {
+   
+}

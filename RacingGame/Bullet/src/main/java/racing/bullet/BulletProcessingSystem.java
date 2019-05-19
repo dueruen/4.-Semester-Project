a9bf2b2775be5c960df02e62d61c888c7f854a8b
dueruen <32197042+dueruen@users.noreply.github.com>
2019-05-19 15:25:33 +0200

    Bullet follow car

--- RacingGame/Bullet/src/main/java/racing/bullet/BulletProcessingSystem.java
+++ RacingGame/Bullet/src/main/java/racing/bullet/BulletProcessingSystem.java
@@ -21,11 +21,8 @@ public class BulletProcessingSystem implements IEntityProcessingService, ItemSPI
             if (entity.getPart(ItemPart.class) != null) {
 
                 ItemPart itemPart = entity.getPart(ItemPart.class);
-                //Shoot if isShooting is true, ie. space is pressed.
                 if (itemPart.isUsing()) {
                     PositionPart positionPart = entity.getPart(PositionPart.class);
-                    //Add entity radius to initial position to avoid immideate collision.
-                   
                     Entity item = createItem(entity, gameData);
                     itemPart.setIsUsing(false);
                     world.addEntity(item);
@@ -40,7 +37,6 @@ public class BulletProcessingSystem implements IEntityProcessingService, ItemSPI
             TimerPart timerPart = bullet.getPart(TimerPart.class);
             movingPart.setUp(true);
             if (timerPart.getExpiration() < 0) {
-                System.out.println("REMOVE");
                 world.removeEntity(bullet);
             }
             timerPart.process(gameData, bullet);
@@ -49,76 +45,28 @@ public class BulletProcessingSystem implements IEntityProcessingService, ItemSPI
         }
     }
 
-//    @Override
-//    public void process(GameData gameData, World world) {
-//        for (Entity player : world.getEntities(Player.class)) {
-//            PositionPart positionPart = player.getPart(PositionPart.class);
-//            MovingPart movingPart = player.getPart(MovingPart.class);
-//            
-//            movingPart.setUp(gameData.getKeys().isDown(GameKeys.SPACE));
-//            
-//            if  (player.getCanShoot() == true){
-//                world.addEntity(createItem(player, gameData));
-//            }
-//            
-//                
-//  
-//            
-//                         
-//                
-//            }
-//        }
     @Override
     public Entity createItem(Entity shooter, GameData gameData) {
         PositionPart shooterPos = shooter.getPart(PositionPart.class);
         MovingPart shooterMovingPart = shooter.getPart(MovingPart.class);
+        GameImage image = shooter.getImage();
 
         float x = shooterPos.getX();
         float y = shooterPos.getY();
         float radians = shooterPos.getRadians();
         float dt = gameData.getDelta();
-        float speed = 350;
+        float speed = 500;
 
         Entity bullet = new Item();
         GameImage img = new GameImage("items/bullet.png", 10, 10);
         bullet.setImage(img);
+
+        float bx = (float) (x + (image.getWidth() / 2) * cos(radians));
+        float by = (float) (y + (image.getWidth() / 2) * sin(radians));
         
-        //bullet.setRadius(2);
-        //float bx = (float) cos(radians) * shooter.getRadius() * bullet.getRadius();
-        //float by = (float) sin(radians) * shooter.getRadius() * bullet.getRadius();
-        //float bx = (float) cos(radians); //* shooter.get * bullet.getRadius();
-        //float by = (float) sin(radians);// * shooter.getRadius() * bullet.getRadius();
-
-        //bullet.add(new PositionPart(bx + x, by + y, radians));
-        bullet.add(new PositionPart(x, y, radians));
-        //bullet.add(new LifePart(1));
-        bullet.add(new MovingPart(0, 50, speed, 5));
+        bullet.add(new PositionPart(bx + (image.getWidth() / 2), by + (image.getHeight()/ 2), radians));
+        bullet.add(new MovingPart(0, 150, speed, 5));
         bullet.add(new TimerPart(1));
-
-//        bullet.setShapeX(new float[2]);
-//        bullet.setShapeY(new float[2]);
         return bullet;
     }
-
-//    public Entity createItem(Entity entity, GameData gameData) {
-//        PositionPart shooterPos = entity.getPart(PositionPart.class);
-//
-//        float x = shooterPos.getX();
-//        float y = shooterPos.getY();
-//        float radians = shooterPos.getRadians();
-//        float speed = 350;
-//        float deacceleration = 10;
-//        float acceleration = 100;
-//        float rotationSpeed = 0;
-//
-//        Entity bulletEntity = new Item();
-//        GameImage img = new GameImage("Itmes/Bullet.png", 100, 50);
-//        bulletEntity.setImage(img);
-//        MovingPart bulletPos = new MovingPart(deacceleration, acceleration, speed, rotationSpeed);
-//        bulletPos.setUp(true);
-//        bulletEntity.add(bulletPos);
-//        bulletEntity.add(new PositionPart(x, y, radians));
-//
-//        return bulletEntity;
-//    }
 }
